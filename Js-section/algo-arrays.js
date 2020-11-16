ad9c8e2da0933ced60c4b2f55da0c8f7f25faa2e
Joe <joseph.lee3454@gmail.com>
2020-11-15 21:11:38 -0800

    added the algos for arrays

---
+++ Js-section/algo-arrays.js
@@ -0,0 +1,30 @@
+// Print Values and Sum
+
+// Print each array value and the sum so far
+// The expected output will be: 
+//var testArr = [6,3,5,1,2,4]
+
+
+var arr = [6,3,5,1,2,4]
+function add(){
+    sum=0
+    for (i=0;i<arr.length; i++){
+        sum = sum + arr[i]
+        console.log("Num "+arr[i],"Sum " + sum)
+} 
+}
+add()
+
+// Value * Position
+
+// Multiply each value in the array by its array position
+// The expected output will be:
+
+
+function mutiply(){
+    for (i=0;i<arr.length; i++){ 
+        multnum = i * arr[i]
+        console.log(multnum)
+    }
+}
+mutiply()
